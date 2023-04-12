1ee93576768fdc1b12da5766b991fab8f0011871
Bartłomiej Wypart <41068795+dintees@users.noreply.github.com>
2023-04-12 08:19:18 +0200

    Add project files.

---
+++ XMLDocumentApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using XMLDocumentLibrary;
+
+Console.WriteLine("Hello, World!");
+
+XMLService Service = new XMLService();
+string conenctionString = @"DATA SOURCE=MSSQLServer;INITIAL CATALOG=BD2_Project;Server=(localdb)\mssqllocaldb";
+
+Console.WriteLine("Connecting...");
+
+if (!Service.Connect(conenctionString))
+{
+    Console.WriteLine("Error occurred while connecting to database!");
+    return;
+}
+
+Console.WriteLine("Connected to dababase!");
+
+Console.WriteLine($"Number of XML documents: {Service.CountDocuments()}");
+
+Service.Disconnect();
---
+++ XMLDocumentLibrary/XMLService.cs
@@ -0,0 +1,43 @@
+﻿using System.Data.SqlClient;
+
+namespace XMLDocumentLibrary
+{
+    public class XMLService
+    {
+        private SqlConnection _connection;
+        public bool Connect(string connectionString)
+        {
+            try
+            {
+                _connection = new SqlConnection(connectionString);
+                _connection.Open();
+                return true;
+            } catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public bool Disconnect()
+        {
+            try
+            {
+                _connection.Close();
+                return true;
+            } catch (Exception e) { return false; }
+        }
+
+        public int CountDocuments()
+        {
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand("SELECT COUNT(*) AS howMany FROM XMLDocument", _connection);
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+                reader.Read();
+                int howMany = (int)reader["howMany"];
+                reader.Close();
+                return howMany;
+            } catch (Exception e) { return -1; }
+        }
+    }
+}
