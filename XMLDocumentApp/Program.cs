3d83175f9276c25de84d7b174fe8720a06086038
Bartłomiej Wypart <41068795+dintees@users.noreply.github.com>
2023-04-12 10:41:38 +0200

    Create, read, edit and delete documents

--- XMLDocumentApp/Program.cs
+++ XMLDocumentApp/Program.cs
@@ -1,13 +1,10 @@
 ﻿using XMLDocumentLibrary;
+using XMLDocumentLibrary.Models;
 
-Console.WriteLine("Hello, World!");
-
-XMLService Service = new XMLService();
 string conenctionString = @"DATA SOURCE=MSSQLServer;INITIAL CATALOG=BD2_Project;Server=(localdb)\mssqllocaldb";
+XMLService XService = new XMLService(conenctionString);
 
-Console.WriteLine("Connecting...");
-
-if (!Service.Connect(conenctionString))
+if (!XService.CheckConnection())
 {
     Console.WriteLine("Error occurred while connecting to database!");
     return;
@@ -15,6 +12,49 @@ if (!Service.Connect(conenctionString))
 
 Console.WriteLine("Connected to dababase!");
 
-Console.WriteLine($"Number of XML documents: {Service.CountDocuments()}");
+Console.WriteLine($"Number of XML documents: {await XService.CountDocumentsAsync()}");
 
-Service.Disconnect();
+var xml = @"<?xml version=""1.0"" encoding=""utf-8"" ?>
+<people>
+<person>
+<fname>Adam</fname>
+<lname>Abacki</lname>
+</person>
+<fname>Bogdan</fname>
+<lname>Babacki</lname>
+</person>
+</people>";
+
+// Console.WriteLine(xml);
+
+// Create document
+/*try
+{
+    bool isCreated = await Service.CreateDocumentAsync("Title", "This is description", xml);
+    Console.WriteLine("Is created? : " + isCreated);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}*/
+
+// Read document with specific id
+XMLDoc? document = XService.GetDocumentById(5);
+if (document is null) Console.WriteLine("There is no document with this ID");
+else
+{
+    Console.WriteLine($"Title: {document.Title}\nDescription: {document.Description}\nContent: {document.XMLDocument}");
+}
+
+// Read all documents
+List<XMLDoc> docs = XService.GetAllDocuments()!;
+foreach (XMLDoc doc in docs)
+{
+    Console.WriteLine($" -- [{doc.Id}] {doc.Title}");
+}
+
+// Edit document parameters
+Console.WriteLine("Modified? : " + XService.ModifyDocument(5, "People", "Better XML document", "<people><person><fname>Adam</fname><lname>Abacki</lname></person></people>"));
+
+// Delete document
+Console.WriteLine("Deleted? : " + XService.DeleteDocument(7));
--- XMLDocumentLibrary/XMLService.cs
+++ XMLDocumentLibrary/XMLService.cs
@@ -1,43 +1,222 @@
 ﻿using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Xml;
+using System.Threading.Tasks;
+using XMLDocumentLibrary.Models;
 
 namespace XMLDocumentLibrary
 {
     public class XMLService
     {
-        private SqlConnection _connection;
-        public bool Connect(string connectionString)
+        private readonly string? _connectionString;
+
+        public XMLService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        /// <summary>
+        /// Validates connection iwth database
+        /// </summary>
+        /// <returns>Bool value represents connectivity state</returns>
+        public bool CheckConnection()
         {
             try
             {
-                _connection = new SqlConnection(connectionString);
-                _connection.Open();
+                SqlConnection connection = new SqlConnection(_connectionString);
+                connection.Open();
+                connection.Close();
                 return true;
-            } catch (Exception e)
+            } catch (Exception)
             {
                 return false;
             }
         }
 
-        public bool Disconnect()
+        /// <summary>
+        /// Counts documents in the database
+        /// </summary>
+        /// <returns>A number of documents. An error occured when -1 was returned</returns>
+        public async Task<int> CountDocumentsAsync()
         {
             try
             {
-                _connection.Close();
-                return true;
-            } catch (Exception e) { return false; }
-        }
-
-        public int CountDocuments()
-        {
-            try
-            {
-                SqlCommand sqlCommand = new SqlCommand("SELECT COUNT(*) AS howMany FROM XMLDocument", _connection);
-                SqlDataReader reader = sqlCommand.ExecuteReader();
+                SqlConnection connection = new SqlConnection(_connectionString);
+                await connection.OpenAsync();
+                SqlCommand sqlCommand = new SqlCommand("SELECT COUNT(*) AS howMany FROM XMLDocument", connection);
+                SqlDataReader reader = await sqlCommand.ExecuteReaderAsync();
                 reader.Read();
                 int howMany = (int)reader["howMany"];
-                reader.Close();
+
+                await reader.CloseAsync();
+                await connection.CloseAsync();
+                
                 return howMany;
-            } catch (Exception e) { return -1; }
+            } catch (Exception) { return -1; }
+        }
+
+        /// <summary>
+        /// Insert document to database
+        /// </summary>
+        /// <param name="title">Title of the document</param>
+        /// <param name="description">Description of the document</param>
+        /// <param name="xmlString">Content of XML document</param>
+        /// <returns>Bool value represents state of inserted document. If true - document has been saved</returns>
+        /// <exception cref="Exception">An error with format of XML</exception>
+        public bool CreateDocument(string title, string description, string xmlString)
+        {
+            try
+            {
+                // validate XML format
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(xmlString);
+
+                // insert document to database
+                int howMany = ExecNonQuery($"INSERT INTO XMLDocument (Title, Description, XDocument) VALUES ('{title}', '{description}', '{xmlString}')");
+                return (howMany > 0);
+            }
+            catch (XmlException e)
+            {
+                throw new Exception("XML format is invalid: " + e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Insert document to database asynchnonously
+        /// </summary>
+        /// <param name="title">Title of the document</param>
+        /// <param name="description">Description of the document</param>
+        /// <param name="xmlString">Content of XML document</param>
+        /// <returns>Bool value represents state of inserted document. If true - document has been saved</returns>
+        /// <exception cref="Exception">An error with format of XML</exception>
+        public async Task<bool> CreateDocumentAsync(string title, string description, string xmlString)
+        {
+            try
+            {
+                // validate XML format
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(xmlString);
+
+                // insert document to database
+                int howMany = await ExecNonQueryAsync($"INSERT INTO XMLDocument (Title, Description, XDocument) VALUES ('{title}', '{description}', '{xmlString}')");
+                return (howMany > 0);
+            }
+            catch (XmlException e)
+            {
+                throw new Exception("XML format is invalid: " + e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Gets all documents from the database
+        /// </summary>
+        /// <returns>List of XMLDoc objects</returns>
+        public List<XMLDoc>? GetAllDocuments()
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(_connectionString);
+                connection.Open();
+                SqlCommand cmd = new SqlCommand("SELECT Id, Title, Description, XDocument FROM XMLDocument", connection);
+                SqlDataReader reader = cmd.ExecuteReader();
+                List<XMLDoc> xDocs = new List<XMLDoc>();
+                while (reader.Read())
+                    xDocs.Add(new XMLDoc { Id = (int)reader["Id"], Title = reader["Title"].ToString(), Description = reader["Description"].ToString(), XMLDocument = reader["XDocument"].ToString() });
+                
+                connection.Close();
+                return xDocs;
+            }
+            catch (Exception) { return null; }
+        }
+
+        /// <summary>
+        /// Gets one document with specific id from the database.
+        /// </summary>
+        /// <param name="id">The id of the document</param>
+        /// <returns>XMLDoc object</returns>
+        public XMLDoc? GetDocumentById(int id)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(_connectionString);
+                connection.Open();
+                SqlCommand cmd = new SqlCommand("SELECT Id, Title, Description, XDocument FROM XMLDocument WHERE id = " + id, connection);
+                SqlDataReader reader = cmd.ExecuteReader();
+                reader.Read();
+                XMLDoc xDoc = new XMLDoc { Id = (int)reader["Id"], Title = reader["Title"].ToString(), Description = reader["Description"].ToString(), XMLDocument = reader["XDocument"].ToString() };
+                connection.Close();
+                return xDoc;
+            } catch (Exception) { return null; }
+        }
+
+        /// <summary>
+        /// Modifies properties of document, like title, description or XMLDocument
+        /// </summary>
+        /// <param name="id">Id of element which will be modified</param>
+        /// <param name="newTitle">New title</param>
+        /// <param name="newDescription">New description</param>
+        /// <param name="newXMLDocument">New XML string document</param>
+        /// <returns>True if document has been edited, false otherwise</returns>
+        public bool ModifyDocument(int id, string? newTitle = null, string? newDescription = null, string? newXMLDocument = null)
+        {
+            if (newXMLDocument is not null)
+            {
+                try
+                {
+                    XmlDocument xmlDoc = new XmlDocument();
+                    xmlDoc.LoadXml(newXMLDocument);
+                }
+                catch (XmlException)
+                {
+                    return false;
+                }
+            }
+
+         string sqlCommand = "UPDATE XMLDocument SET"
+            + (newTitle is not null ? " Title = '" + newTitle + "'" : "")
+            + (newDescription is not null ? ", Description = '" + newDescription + "'" : "")
+            + (newXMLDocument is not null ? ", XDocument = '" + newXMLDocument + "'" : "")
+            + " WHERE id = " + id;
+
+            return (ExecNonQuery(sqlCommand) > 0);
+        }
+
+        /// <summary>
+        /// Deletes one document with specified id from the database
+        /// </summary>
+        /// <param name="id">Id of the document</param>
+        /// <returns>True if document has been deleted</returns>
+        public bool DeleteDocument(int id)
+        {
+            return (ExecNonQuery("DELETE FROM XMLDocument WHERE id = " + id) > 0);
+        }
+
+
+        private int ExecNonQuery(string command)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(_connectionString);
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(command, connection);
+                int howMany = cmd.ExecuteNonQuery();
+                connection.Close();
+                return howMany;
+            }
+            catch (Exception) { return -1; }
+        }
+
+        private async Task<int> ExecNonQueryAsync(string command)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(_connectionString);
+                await connection.OpenAsync();
+                SqlCommand cmd = new SqlCommand(command, connection);
+                int howMany = await cmd.ExecuteNonQueryAsync();
+                await connection.CloseAsync();
+                return howMany;
+            } catch (Exception) { return -1; }
         }
     }
 }
